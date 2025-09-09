587adce6a84140edfb960aed7ed0b45e21024ff4
jubayer718 <jubayerahmed2462@gmail.com>
2025-09-09 06:50:18 +0600

    initial project setup and layout system done

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/index.tsx";
+import { ThemeProvider } from "./providers/theme.provider.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <RouterProvider router={router} />
+
+    </ThemeProvider>
+  </React.StrictMode>
+);
