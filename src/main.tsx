e423c0b5c6b73670b7d187f72c996e15e7a05a09
jubayer718 <jubayerahmed2462@gmail.com>
2025-09-09 08:25:36 +0600

    login and register form

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
 import { ThemeProvider } from "./providers/theme.provider.tsx";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <RouterProvider router={router} />
-
+    <Toaster richColors/>
     </ThemeProvider>
   </React.StrictMode>
 );
